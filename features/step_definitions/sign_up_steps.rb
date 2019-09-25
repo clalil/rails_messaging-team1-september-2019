3d7ba76750f9bd4db1492c0e156e01f94186bb85
clalil <cliljander@gmail.com>
2019-09-25 10:57:47 +0200

    Add signup name key to params

--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -7,7 +7,7 @@ Given("I visit the {string} page") do |string|
   end
   
   When("I fill in {string} with {string}") do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
+    expect(page).to have_content string2
   end
   
   When("I click {string} button") do |string|
